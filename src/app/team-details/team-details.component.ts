cd94b58008539bfa811d6063dcacd178ecf2318b
Pierre <sim.pierre91@gmail.com>
2019-04-24 00:05:03 +0200

    mock team and team detail view

--- src/app/team-details/team-details.component.ts
+++ src/app/team-details/team-details.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Area {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-team-details',
   templateUrl: './team-details.component.html',
   styleUrls: ['./team-details.component.scss']
 })
+
 export class TeamDetailsComponent implements OnInit {
+
+  areas: Area[] = [
+    {id: 1, name: 'M1'},
+    {id: 2, name: 'M2'},
+    {id: 3, name: 'M3'}
+  ];
 
   constructor() { }
 
